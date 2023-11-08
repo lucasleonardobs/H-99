0b2b3917520c95f71a5686983390b3e2cb6ff9fa
Lucas Leonardo <lucas.leonardo>
2023-11-08 14:48:37 -0300

    Find the list lenght

--- exercises/lists.hs
+++ exercises/lists.hs
@@ -18,3 +18,9 @@ findK :: Int -> [a] -> a
 findK _ [] = error "empty list"
 findK 1 (x:_) = x
 findK k (x:xs) = findK (k-1) xs
+
+-- 4. Find the number of elements of a list.
+
+lenght :: [a] -> Int
+lenght [] = 0
+lenght (x:xs) = 1 + lenght xs
