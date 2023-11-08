f45fb54360ca6c86776dc90be0f818b104f29a98
Lucas Leonardo <lucas.leonardo>
2023-11-08 14:24:10 -0300

    Add error handling to empty lists in ex 1 and 2

--- exercises/lists.hs
+++ exercises/lists.hs
@@ -1,10 +1,12 @@
 -- 1. Find the last element of a list.
 
 myLast :: [a] -> a
+myLast [] = error "empty list"
 myLast [x] = x
 myLast (x:xs) = myLast xs
 
 -- 2. Find the last but one element of a list.
 myLastButOne :: [a] -> a
+myLastButOne [] = error "empty list"
 myLastButOne [x, y] = x
 myLastButOne (x:xs) = myLastButOne xs
