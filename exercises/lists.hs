0e4c02753082ac996653328d171476c2ac10d483
Lucas Leonardo <lucas.leonardo>
2023-11-08 14:21:50 -0300

    Find last but one element

--- exercises/lists.hs
+++ exercises/lists.hs
@@ -1,5 +1,10 @@
--- Find the last element of a list.
+-- 1. Find the last element of a list.
 
 myLast :: [a] -> a
 myLast [x] = x
 myLast (x:xs) = myLast xs
+
+-- 2. Find the last but one element of a list.
+myLastButOne :: [a] -> a
+myLastButOne [x, y] = x
+myLastButOne (x:xs) = myLastButOne xs
